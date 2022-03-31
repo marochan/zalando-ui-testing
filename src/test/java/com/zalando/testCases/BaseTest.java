0c92523860764fad7196872fac5bd4333847e5ae
Cezary Rokita <65018003+marochan@users.noreply.github.com>
2022-03-31 16:00:33 +0200

    Deleted unnecessary logger

--- src/test/java/com/zalando/testCases/BaseTest.java
+++ src/test/java/com/zalando/testCases/BaseTest.java
@@ -1,18 +1,15 @@
 package com.zalando.testCases;
 
 import com.zalando.steps.PropertiesHandler;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 
 import java.io.IOException;
 
 public class BaseTest {
-    protected static Logger logger = LoggerFactory.getLogger("Zalando-logger");
+    
     @BeforeClass
     public static void setUp() {
-        logger.error("Entering setup method");
         PropertiesHandler.getProperties();
     }
 
